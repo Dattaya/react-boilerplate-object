7f40e0bb44c9aff0df1edef9f0fa59a6939ffdf6
Yaroslav Kiliba <om.dattaya@gmail.com>
2016-12-29 22:29:02 +0200

    chore(graphql): remove useless code

--- graphql/middlewares/subscriptionMiddleware.js
+++ graphql/middlewares/subscriptionMiddleware.js
@@ -7,19 +7,6 @@ module.exports = (server, schema) => {
   // More advanced example: https://github.com/apollostack/GitHunt-API/blob/master/api/subscriptions.js
   const subscriptionManager = new SubscriptionManager({ schema, pubsub });
 
-  subscriptionManager.subscribe({
-    query: `
-      subscription FreshTimeSubscription {
-        freshTime
-      }
-    `,
-    callback: (err) => {
-      if (err) {
-        console.error(err); // eslint-disable-line no-console
-      }
-    },
-  });
-
   return new SubscriptionServer(
     { subscriptionManager },
     server
